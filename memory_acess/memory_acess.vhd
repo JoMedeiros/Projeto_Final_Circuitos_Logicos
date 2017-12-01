0b157f12f34ce3aec4eeca5751247f7d012cdf83
JosivanMedeiros <josivan.medeiros@live.com>
2017-12-01 10:10:11 -0200

    links

--- memory_acess/memory_acess.vhd
+++ memory_acess/memory_acess.vhd
@@ -5,14 +5,19 @@ USE ieee.numeric_std.all;
 ENTITY memory_acess IS
     port (
        clk  : in std_logic;
-		  
-       
+		 Executa: in std_logic;
+       OPcode : in std_logic_vector( 3 downto 0 );
+		 instrucao : out std_logic_vector( 15 downto 0 )
     );
-end ram;
+end memory_acess;
 
 architecture arch of memory_acess is
 
 COMPONENT ram IS
+	generic (
+        DATA_WIDTH : integer := 16;
+        ADDR_WIDTH : integer := 16
+    );
 	PORT (
         clock  : in std_logic;
         wren   : in std_logic; -- Sempre 0
@@ -22,8 +27,29 @@ COMPONENT ram IS
     );
 END COMPONENT;
 
-BEGIN
+type Tcpu_state is (
+	ESPERA,	-- Esperando executar
+	VERIFICA,	-- Verifica o tipo de instrução
+	DECOD_ADD, DECOD_SUB, DECOD_AND,	-- decodificação das instruções
+	DECOD_NOR, DECOD_OR, DECOD_SHL,
+	DECOD_SHR, DECOD_LW, DECOD_SW, 
+	DECOD_ADDi				
+	);
+VARIABLE ESTADO_CPU : TCPU_STATE;  -- variável de estado da CPU (CPU state variable)
+
+signal iaddr : std_logic_vector( 15 downto 0 );
+
+begin
 	
-	memi : PORT MAP (clk, );
+	process (clock)
+    begin
+        if clock'event and clock = '1' and executa = '1' then
+            if Executa = '1' then
+                ram_image(to_integer(unsigned(addr))) <= data_i;
+            end if;
+        end if;
+    end process;
+	
+	memi : PORT MAP (clk, '0', );
     
 end arch;
